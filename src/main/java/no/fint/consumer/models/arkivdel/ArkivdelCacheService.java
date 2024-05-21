ba2ddf7de48c05d2ed9abb7f5c0dd8fb396717df
Hknots <hknots99@gmail.com>
2024-05-21 12:30:49 +0200

    manual changes

--- src/main/java/no/fint/consumer/models/arkivdel/ArkivdelCacheService.java
+++ src/main/java/no/fint/consumer/models/arkivdel/ArkivdelCacheService.java
@@ -107,7 +107,10 @@ public class ArkivdelCacheService extends CacheService<ArkivdelResource> {
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (NoarkActions.valueOf(event.getAction()) == NoarkActions.UPDATE_ARKIVDEL) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<ArkivdelResource>> cacheObjects = data
