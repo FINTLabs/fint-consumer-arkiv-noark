c2b9471d13d6ca9a43deb97fc890641542566bf9
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-01-15 15:28:24 +0100

    Version 4.0.0-rc-6.

---
+++ src/main/java/no/novari/fint/consumer/models/arkivressurs/ArkivressursController.java
@@ -0,0 +1,369 @@
+package no.novari.fint.consumer.models.arkivressurs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.collect.ImmutableMap;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import no.fint.audit.FintAuditService;
+
+import no.novari.fint.consumer.config.Constants;
+import no.novari.fint.consumer.config.ConsumerProps;
+import no.novari.fint.consumer.event.ConsumerEventUtil;
+import no.novari.fint.consumer.event.SynchronousEvents;
+import no.novari.fint.consumer.exceptions.*;
+import no.novari.fint.consumer.status.StatusCache;
+import no.novari.fint.consumer.utils.EventResponses;
+import no.novari.fint.consumer.utils.RestEndpoints;
+import no.fint.antlr.FintFilterService;
+
+import no.fint.event.model.*;
+
+import no.novari.fint.relations.FintRelationsMediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import no.novari.fint.model.resource.arkiv.noark.ArkivressursResource;
+import no.novari.fint.model.resource.arkiv.noark.ArkivressursResources;
+import no.novari.fint.model.arkiv.noark.NoarkActions;
+
+@Slf4j
+@Api(tags = {"Arkivressurs"})
+@CrossOrigin
+@RestController
+@RequestMapping(name = "Arkivressurs", value = RestEndpoints.ARKIVRESSURS, produces = {FintRelationsMediaType.APPLICATION_HAL_JSON_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+public class ArkivressursController {
+
+    private static final String ODATA_FILTER_QUERY_OPTION = "$filter=";
+
+    @Autowired(required = false)
+    private ArkivressursCacheService cacheService;
+
+    @Autowired
+    private FintAuditService fintAuditService;
+
+    @Autowired
+    private ArkivressursLinker linker;
+
+    @Autowired
+    private ConsumerProps props;
+
+    @Autowired
+    private StatusCache statusCache;
+
+    @Autowired
+    private ConsumerEventUtil consumerEventUtil;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private SynchronousEvents synchronousEvents;
+
+    @Autowired
+    private FintFilterService fintFilterService;
+
+    @GetMapping("/last-updated")
+    public Map<String, String> getLastUpdated(@RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId) {
+        if (cacheService == null) {
+            throw new CacheDisabledException("Arkivressurs cache is disabled.");
+        }
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        String lastUpdated = Long.toString(cacheService.getLastUpdated(orgId));
+        return ImmutableMap.of("lastUpdated", lastUpdated);
+    }
+
+    @GetMapping("/cache/size")
+    public ImmutableMap<String, Integer> getCacheSize(@RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId) {
+        if (cacheService == null) {
+            throw new CacheDisabledException("Arkivressurs cache is disabled.");
+        }
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        return ImmutableMap.of("size", cacheService.getCacheSize(orgId));
+    }
+
+    @GetMapping
+    public ArkivressursResources getArkivressurs(
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client,
+            @RequestParam(defaultValue = "0") long sinceTimeStamp,
+            @RequestParam(defaultValue = "0") int size,
+            @RequestParam(defaultValue = "0") int offset,
+            @RequestParam(required = false) String $filter,
+            HttpServletRequest request) throws InterruptedException {
+        if (cacheService == null) {
+            if (StringUtils.isNotBlank($filter)) {
+                return getArkivressursByOdataFilter(client, orgId, $filter);
+            }
+            throw new CacheDisabledException("Arkivressurs cache is disabled.");
+        }
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        if (client == null) {
+            client = props.getDefaultClient();
+        }
+        log.debug("OrgId: {}, Client: {}", orgId, client);
+
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.GET_ALL_ARKIVRESSURS, client);
+        event.setOperation(Operation.READ);
+        if (StringUtils.isNotBlank(request.getQueryString())) {
+            event.setQuery("?" + request.getQueryString());
+        }
+        fintAuditService.audit(event);
+        fintAuditService.audit(event, Status.CACHE);
+
+        Stream<ArkivressursResource> resources;
+        if (size > 0 && offset >= 0 && sinceTimeStamp > 0) {
+            resources = cacheService.streamSliceSince(orgId, sinceTimeStamp, offset, size);
+        } else if (size > 0 && offset >= 0) {
+            resources = cacheService.streamSlice(orgId, offset, size);
+        } else if (sinceTimeStamp > 0) {
+            resources = cacheService.streamSince(orgId, sinceTimeStamp);
+        } else {
+            resources = cacheService.streamAll(orgId);
+        }
+
+        fintAuditService.audit(event, Status.CACHE_RESPONSE, Status.SENT_TO_CLIENT);
+
+        return linker.toResources(resources, offset, size, cacheService.getCacheSize(orgId));
+    }
+    
+    @PostMapping("/$query")
+    public ArkivressursResources getArkivressursByQuery(
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false)   String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client,
+            @RequestParam(defaultValue = "0") long sinceTimeStamp,
+            @RequestParam(defaultValue = "0") int  size,
+            @RequestParam(defaultValue = "0") int  offset,
+            @RequestBody(required = false) String query,
+            HttpServletRequest request
+    ) throws InterruptedException {
+        return getArkivressurs(orgId, client, sinceTimeStamp, size, offset, query, request);
+    }
+
+    private ArkivressursResources getArkivressursByOdataFilter(
+        String client, String orgId, String $filter
+    ) throws InterruptedException {
+        if (!fintFilterService.validate($filter))
+            throw new IllegalArgumentException("OData Filter is not valid");
+    
+        if (props.isOverrideOrgId() || orgId == null) orgId = props.getDefaultOrgId();
+        if (client == null) client = props.getDefaultClient();
+    
+        Event event = new Event(
+                orgId, Constants.COMPONENT,
+                NoarkActions.GET_ARKIVRESSURS, client);
+        event.setOperation(Operation.READ);
+        event.setQuery(ODATA_FILTER_QUERY_OPTION.concat($filter));
+    
+        BlockingQueue<Event> queue = synchronousEvents.register(event);
+        consumerEventUtil.send(event);
+    
+        Event response = EventResponses.handle(queue.poll(5, TimeUnit.MINUTES));
+        if (response.getData() == null || response.getData().isEmpty())
+            return new ArkivressursResources();
+    
+        ArrayList<ArkivressursResource> list = objectMapper.convertValue(
+                response.getData(),
+                new TypeReference<ArrayList<ArkivressursResource>>() {});
+        fintAuditService.audit(response, Status.SENT_TO_CLIENT);
+        list.forEach(r -> linker.mapAndResetLinks(r));
+        return linker.toResources(list);
+    }
+
+
+    @GetMapping("/kildesystemid/{id:.+}")
+    public ArkivressursResource getArkivressursByKildesystemId(
+            @PathVariable String id,
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client) throws InterruptedException {
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        if (client == null) {
+            client = props.getDefaultClient();
+        }
+        log.debug("kildesystemId: {}, OrgId: {}, Client: {}", id, orgId, client);
+
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.GET_ARKIVRESSURS, client);
+        event.setOperation(Operation.READ);
+        event.setQuery("kildesystemId/" + id);
+
+        if (cacheService != null) {
+            fintAuditService.audit(event);
+            fintAuditService.audit(event, Status.CACHE);
+
+            Optional<ArkivressursResource> arkivressurs = cacheService.getArkivressursByKildesystemId(orgId, id);
+
+            fintAuditService.audit(event, Status.CACHE_RESPONSE, Status.SENT_TO_CLIENT);
+
+            return arkivressurs.map(linker::toResource).orElseThrow(() -> new EntityNotFoundException(id));
+
+        } else {
+            BlockingQueue<Event> queue = synchronousEvents.register(event);
+            consumerEventUtil.send(event);
+
+            Event response = EventResponses.handle(queue.poll(5, TimeUnit.MINUTES));
+
+            if (response.getData() == null ||
+                    response.getData().isEmpty()) throw new EntityNotFoundException(id);
+
+            ArkivressursResource arkivressurs = objectMapper.convertValue(response.getData().get(0), ArkivressursResource.class);
+
+            fintAuditService.audit(response, Status.SENT_TO_CLIENT);
+
+            return linker.mapAndResetLinks(arkivressurs);
+        }    
+    }
+
+    @GetMapping("/systemid/{id:.+}")
+    public ArkivressursResource getArkivressursBySystemId(
+            @PathVariable String id,
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client) throws InterruptedException {
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        if (client == null) {
+            client = props.getDefaultClient();
+        }
+        log.debug("systemId: {}, OrgId: {}, Client: {}", id, orgId, client);
+
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.GET_ARKIVRESSURS, client);
+        event.setOperation(Operation.READ);
+        event.setQuery("systemId/" + id);
+
+        if (cacheService != null) {
+            fintAuditService.audit(event);
+            fintAuditService.audit(event, Status.CACHE);
+
+            Optional<ArkivressursResource> arkivressurs = cacheService.getArkivressursBySystemId(orgId, id);
+
+            fintAuditService.audit(event, Status.CACHE_RESPONSE, Status.SENT_TO_CLIENT);
+
+            return arkivressurs.map(linker::toResource).orElseThrow(() -> new EntityNotFoundException(id));
+
+        } else {
+            BlockingQueue<Event> queue = synchronousEvents.register(event);
+            consumerEventUtil.send(event);
+
+            Event response = EventResponses.handle(queue.poll(5, TimeUnit.MINUTES));
+
+            if (response.getData() == null ||
+                    response.getData().isEmpty()) throw new EntityNotFoundException(id);
+
+            ArkivressursResource arkivressurs = objectMapper.convertValue(response.getData().get(0), ArkivressursResource.class);
+
+            fintAuditService.audit(response, Status.SENT_TO_CLIENT);
+
+            return linker.mapAndResetLinks(arkivressurs);
+        }    
+    }
+
+
+
+    // Writable class
+    @GetMapping("/status/{id}")
+    public ResponseEntity getStatus(
+            @PathVariable String id,
+            @RequestHeader(HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(HeaderConstants.CLIENT) String client) {
+        log.debug("/status/{} for {} from {}", id, orgId, client);
+        return statusCache.handleStatusRequest(id, orgId, linker, ArkivressursResource.class);
+    }
+
+    @PostMapping
+    public ResponseEntity postArkivressurs(
+            @RequestHeader(name = HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT) String client,
+            @RequestBody ArkivressursResource body,
+            @RequestParam(name = "validate", required = false) boolean validate
+    ) {
+        log.debug("postArkivressurs, Validate: {}, OrgId: {}, Client: {}", validate, orgId, client);
+        log.trace("Body: {}", body);
+        linker.mapLinks(body);
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.UPDATE_ARKIVRESSURS, client);
+        event.addObject(objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS).convertValue(body, Map.class));
+        event.setOperation(validate ? Operation.VALIDATE : Operation.CREATE);
+        consumerEventUtil.send(event);
+
+        statusCache.put(event.getCorrId(), event);
+
+        URI location = UriComponentsBuilder.fromUriString(linker.self()).path("status/{id}").buildAndExpand(event.getCorrId()).toUri();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).location(location).build();
+    }
+
+  
+    @PutMapping("/kildesystemid/{id:.+}")
+    public ResponseEntity putArkivressursByKildesystemId(
+            @PathVariable String id,
+            @RequestHeader(name = HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT) String client,
+            @RequestBody ArkivressursResource body
+    ) {
+        log.debug("putArkivressursByKildesystemId {}, OrgId: {}, Client: {}", id, orgId, client);
+        log.trace("Body: {}", body);
+        linker.mapLinks(body);
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.UPDATE_ARKIVRESSURS, client);
+        event.setQuery("kildesystemid/" + id);
+        event.addObject(objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS).convertValue(body, Map.class));
+        event.setOperation(Operation.UPDATE);
+        fintAuditService.audit(event);
+
+        consumerEventUtil.send(event);
+
+        statusCache.put(event.getCorrId(), event);
+
+        URI location = UriComponentsBuilder.fromUriString(linker.self()).path("status/{id}").buildAndExpand(event.getCorrId()).toUri();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).location(location).build();
+    }
+  
+    @PutMapping("/systemid/{id:.+}")
+    public ResponseEntity putArkivressursBySystemId(
+            @PathVariable String id,
+            @RequestHeader(name = HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT) String client,
+            @RequestBody ArkivressursResource body
+    ) {
+        log.debug("putArkivressursBySystemId {}, OrgId: {}, Client: {}", id, orgId, client);
+        log.trace("Body: {}", body);
+        linker.mapLinks(body);
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.UPDATE_ARKIVRESSURS, client);
+        event.setQuery("systemid/" + id);
+        event.addObject(objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS).convertValue(body, Map.class));
+        event.setOperation(Operation.UPDATE);
+        fintAuditService.audit(event);
+
+        consumerEventUtil.send(event);
+
+        statusCache.put(event.getCorrId(), event);
+
+        URI location = UriComponentsBuilder.fromUriString(linker.self()).path("status/{id}").buildAndExpand(event.getCorrId()).toUri();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).location(location).build();
+    }
+  
+}
+
---
+++ src/main/java/no/novari/fint/consumer/models/arkivressurs/ArkivressursLinker.java
@@ -0,0 +1,70 @@
+package no.novari.fint.consumer.models.arkivressurs;
+
+import no.novari.fint.model.resource.arkiv.noark.ArkivressursResource;
+import no.novari.fint.model.resource.arkiv.noark.ArkivressursResources;
+import no.novari.fint.relations.FintLinker;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Objects.isNull;
+import static org.springframework.util.StringUtils.isEmpty;
+
+@Component
+public class ArkivressursLinker extends FintLinker<ArkivressursResource> {
+
+    public ArkivressursLinker() {
+        super(ArkivressursResource.class);
+    }
+
+    public void mapLinks(ArkivressursResource resource) {
+        super.mapLinks(resource);
+    }
+
+    @Override
+    public ArkivressursResources toResources(Collection<ArkivressursResource> collection) {
+        return toResources(collection.stream(), 0, 0, collection.size());
+    }
+
+    @Override
+    public ArkivressursResources toResources(Stream<ArkivressursResource> stream, int offset, int size, int totalItems) {
+        ArkivressursResources resources = new ArkivressursResources();
+        stream.map(this::toResource).forEach(resources::addResource);
+        addPagination(resources, offset, size, totalItems);
+        return resources;
+    }
+
+    @Override
+    public String getSelfHref(ArkivressursResource arkivressurs) {
+        return getAllSelfHrefs(arkivressurs).findFirst().orElse(null);
+    }
+
+    @Override
+    public Stream<String> getAllSelfHrefs(ArkivressursResource arkivressurs) {
+        Stream.Builder<String> builder = Stream.builder();
+        if (!isNull(arkivressurs.getKildesystemId()) && !isEmpty(arkivressurs.getKildesystemId().getIdentifikatorverdi())) {
+            builder.add(createHrefWithId(arkivressurs.getKildesystemId().getIdentifikatorverdi(), "kildesystemid"));
+        }
+        if (!isNull(arkivressurs.getSystemId()) && !isEmpty(arkivressurs.getSystemId().getIdentifikatorverdi())) {
+            builder.add(createHrefWithId(arkivressurs.getSystemId().getIdentifikatorverdi(), "systemid"));
+        }
+        
+        return builder.build();
+    }
+
+    int[] hashCodes(ArkivressursResource arkivressurs) {
+        IntStream.Builder builder = IntStream.builder();
+        if (!isNull(arkivressurs.getKildesystemId()) && !isEmpty(arkivressurs.getKildesystemId().getIdentifikatorverdi())) {
+            builder.add(arkivressurs.getKildesystemId().getIdentifikatorverdi().hashCode());
+        }
+        if (!isNull(arkivressurs.getSystemId()) && !isEmpty(arkivressurs.getSystemId().getIdentifikatorverdi())) {
+            builder.add(arkivressurs.getSystemId().getIdentifikatorverdi().hashCode());
+        }
+        
+        return builder.build().toArray();
+    }
+
+}
+
