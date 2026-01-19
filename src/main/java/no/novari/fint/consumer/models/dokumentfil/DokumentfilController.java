4371e854be7c6e54c6878c58ecf66027e87cb278
Sander Nilsen Rysjedal <117441169+Sander14121@users.noreply.github.com>
2026-01-19 12:15:00 +0100

    Version 4.0.0-rc-6. (#42)
    * Version 4.0.0-rc-6.
    
    * refactor and add odatafilter

---
+++ src/main/java/no/novari/fint/consumer/models/dokumentfil/DokumentfilController.java
@@ -0,0 +1,347 @@
+package no.novari.fint.consumer.models.dokumentfil;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.common.collect.ImmutableMap;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import no.fint.audit.FintAuditService;
+
+import no.fint.cache.exceptions.*;
+import no.novari.fint.consumer.config.Constants;
+import no.novari.fint.consumer.config.ConsumerProps;
+import no.novari.fint.consumer.event.ConsumerEventUtil;
+import no.novari.fint.consumer.event.SynchronousEvents;
+import no.novari.fint.consumer.exceptions.*;
+import no.novari.fint.consumer.status.StatusCache;
+import no.novari.fint.consumer.utils.EventResponses;
+import no.novari.fint.consumer.utils.RestEndpoints;
+import no.fint.antlr.FintFilterService;
+
+import no.fint.event.model.*;
+
+import no.novari.fint.relations.FintRelationsMediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.net.UnknownHostException;
+
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import no.novari.fint.model.resource.arkiv.noark.DokumentfilResource;
+import no.novari.fint.model.resource.arkiv.noark.DokumentfilResources;
+import no.novari.fint.model.arkiv.noark.NoarkActions;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Slf4j
+@Api(tags = {"Dokumentfil"})
+@CrossOrigin
+@RestController
+@RequestMapping(name = "Dokumentfil", value = RestEndpoints.DOKUMENTFIL, produces = {FintRelationsMediaType.APPLICATION_HAL_JSON_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+public class DokumentfilController {
+
+    private static final String ODATA_FILTER_QUERY_OPTION = "$filter=";
+
+    @Autowired(required = false)
+    private DokumentfilCacheService cacheService;
+
+    @Autowired
+    private FintAuditService fintAuditService;
+
+    @Autowired
+    private DokumentfilLinker linker;
+
+    @Autowired
+    private ConsumerProps props;
+
+    @Autowired
+    private StatusCache statusCache;
+
+    @Autowired
+    private ConsumerEventUtil consumerEventUtil;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private SynchronousEvents synchronousEvents;
+
+    @Autowired
+    private FintFilterService fintFilterService;
+
+    @GetMapping("/last-updated")
+    public Map<String, String> getLastUpdated(@RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId) {
+        if (cacheService == null) {
+            throw new CacheDisabledException("Dokumentfil cache is disabled.");
+        }
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        String lastUpdated = Long.toString(cacheService.getLastUpdated(orgId));
+        return ImmutableMap.of("lastUpdated", lastUpdated);
+    }
+
+    @GetMapping("/cache/size")
+    public ImmutableMap<String, Integer> getCacheSize(@RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId) {
+        if (cacheService == null) {
+            throw new CacheDisabledException("Dokumentfil cache is disabled.");
+        }
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        return ImmutableMap.of("size", cacheService.getCacheSize(orgId));
+    }
+
+    @GetMapping
+    public DokumentfilResources getDokumentfil(
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client,
+            @RequestParam(defaultValue = "0") long sinceTimeStamp,
+            @RequestParam(defaultValue = "0") int size,
+            @RequestParam(defaultValue = "0") int offset,
+            @RequestParam(required = false) String $filter,
+            HttpServletRequest request) throws InterruptedException {
+        if (cacheService == null) {
+            if (StringUtils.isNotBlank($filter)) {
+                return getDokumentfilByOdataFilter(client, orgId, $filter);
+            }
+            throw new CacheDisabledException("Dokumentfil cache is disabled.");
+        }
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        if (client == null) {
+            client = props.getDefaultClient();
+        }
+        log.debug("OrgId: {}, Client: {}", orgId, client);
+
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.GET_ALL_DOKUMENTFIL, client);
+        event.setOperation(Operation.READ);
+        if (StringUtils.isNotBlank(request.getQueryString())) {
+            event.setQuery("?" + request.getQueryString());
+        }
+        fintAuditService.audit(event);
+        fintAuditService.audit(event, Status.CACHE);
+
+        Stream<DokumentfilResource> resources;
+        if (size > 0 && offset >= 0 && sinceTimeStamp > 0) {
+            resources = cacheService.streamSliceSince(orgId, sinceTimeStamp, offset, size);
+        } else if (size > 0 && offset >= 0) {
+            resources = cacheService.streamSlice(orgId, offset, size);
+        } else if (sinceTimeStamp > 0) {
+            resources = cacheService.streamSince(orgId, sinceTimeStamp);
+        } else {
+            resources = cacheService.streamAll(orgId);
+        }
+
+        fintAuditService.audit(event, Status.CACHE_RESPONSE, Status.SENT_TO_CLIENT);
+
+        return linker.toResources(resources, offset, size, cacheService.getCacheSize(orgId));
+    }
+    
+    @PostMapping("/$query")
+    public DokumentfilResources getDokumentfilByQuery(
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false)   String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client,
+            @RequestParam(defaultValue = "0") long sinceTimeStamp,
+            @RequestParam(defaultValue = "0") int  size,
+            @RequestParam(defaultValue = "0") int  offset,
+            @RequestBody(required = false) String query,
+            HttpServletRequest request
+    ) throws InterruptedException {
+        return getDokumentfil(orgId, client, sinceTimeStamp, size, offset, query, request);
+    }
+
+    private DokumentfilResources getDokumentfilByOdataFilter(
+        String client, String orgId, String $filter
+    ) throws InterruptedException {
+        if (!fintFilterService.validate($filter))
+            throw new IllegalArgumentException("OData Filter is not valid");
+    
+        if (props.isOverrideOrgId() || orgId == null) orgId = props.getDefaultOrgId();
+        if (client == null) client = props.getDefaultClient();
+    
+        Event event = new Event(
+                orgId, Constants.COMPONENT,
+                NoarkActions.GET_DOKUMENTFIL, client);
+        event.setOperation(Operation.READ);
+        event.setQuery(ODATA_FILTER_QUERY_OPTION.concat($filter));
+    
+        BlockingQueue<Event> queue = synchronousEvents.register(event);
+        consumerEventUtil.send(event);
+    
+        Event response = EventResponses.handle(queue.poll(5, TimeUnit.MINUTES));
+        if (response.getData() == null || response.getData().isEmpty())
+            return new DokumentfilResources();
+    
+        ArrayList<DokumentfilResource> list = objectMapper.convertValue(
+                response.getData(),
+                new TypeReference<ArrayList<DokumentfilResource>>() {});
+        fintAuditService.audit(response, Status.SENT_TO_CLIENT);
+        list.forEach(r -> linker.mapAndResetLinks(r));
+        return linker.toResources(list);
+    }
+
+
+    @GetMapping("/systemid/{id:.+}")
+    public DokumentfilResource getDokumentfilBySystemId(
+            @PathVariable String id,
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client) throws InterruptedException {
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        if (client == null) {
+            client = props.getDefaultClient();
+        }
+        log.debug("systemId: {}, OrgId: {}, Client: {}", id, orgId, client);
+
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.GET_DOKUMENTFIL, client);
+        event.setOperation(Operation.READ);
+        event.setQuery("systemId/" + id);
+
+        if (cacheService != null) {
+            fintAuditService.audit(event);
+            fintAuditService.audit(event, Status.CACHE);
+
+            Optional<DokumentfilResource> dokumentfil = cacheService.getDokumentfilBySystemId(orgId, id);
+
+            fintAuditService.audit(event, Status.CACHE_RESPONSE, Status.SENT_TO_CLIENT);
+
+            return dokumentfil.map(linker::toResource).orElseThrow(() -> new EntityNotFoundException(id));
+
+        } else {
+            BlockingQueue<Event> queue = synchronousEvents.register(event);
+            consumerEventUtil.send(event);
+
+            Event response = EventResponses.handle(queue.poll(5, TimeUnit.MINUTES));
+
+            if (response.getData() == null ||
+                    response.getData().isEmpty()) throw new EntityNotFoundException(id);
+
+            DokumentfilResource dokumentfil = objectMapper.convertValue(response.getData().get(0), DokumentfilResource.class);
+
+            fintAuditService.audit(response, Status.SENT_TO_CLIENT);
+
+            return linker.mapAndResetLinks(dokumentfil);
+        }    
+    }
+
+
+
+    // Writable class
+    @GetMapping("/status/{id}")
+    public ResponseEntity getStatus(
+            @PathVariable String id,
+            @RequestHeader(HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(HeaderConstants.CLIENT) String client) {
+        log.debug("/status/{} for {} from {}", id, orgId, client);
+        return statusCache.handleStatusRequest(id, orgId, linker, DokumentfilResource.class);
+    }
+
+    @PostMapping
+    public ResponseEntity postDokumentfil(
+            @RequestHeader(name = HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT) String client,
+            @RequestBody DokumentfilResource body,
+            @RequestParam(name = "validate", required = false) boolean validate
+    ) {
+        log.debug("postDokumentfil, Validate: {}, OrgId: {}, Client: {}", validate, orgId, client);
+        log.trace("Body: {}", body);
+        linker.mapLinks(body);
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.UPDATE_DOKUMENTFIL, client);
+        event.addObject(objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS).convertValue(body, Map.class));
+        event.setOperation(validate ? Operation.VALIDATE : Operation.CREATE);
+        consumerEventUtil.send(event);
+
+        statusCache.put(event.getCorrId(), event);
+
+        URI location = UriComponentsBuilder.fromUriString(linker.self()).path("status/{id}").buildAndExpand(event.getCorrId()).toUri();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).location(location).build();
+    }
+
+  
+    @PutMapping("/systemid/{id:.+}")
+    public ResponseEntity putDokumentfilBySystemId(
+            @PathVariable String id,
+            @RequestHeader(name = HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT) String client,
+            @RequestBody DokumentfilResource body
+    ) {
+        log.debug("putDokumentfilBySystemId {}, OrgId: {}, Client: {}", id, orgId, client);
+        log.trace("Body: {}", body);
+        linker.mapLinks(body);
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.UPDATE_DOKUMENTFIL, client);
+        event.setQuery("systemid/" + id);
+        event.addObject(objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS).convertValue(body, Map.class));
+        event.setOperation(Operation.UPDATE);
+        fintAuditService.audit(event);
+
+        consumerEventUtil.send(event);
+
+        statusCache.put(event.getCorrId(), event);
+
+        URI location = UriComponentsBuilder.fromUriString(linker.self()).path("status/{id}").buildAndExpand(event.getCorrId()).toUri();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).location(location).build();
+    }
+  
+
+    //
+    // Exception handlers
+    //
+    @ExceptionHandler(EventResponseException.class)
+    public ResponseEntity handleEventResponseException(EventResponseException e) {
+        return ResponseEntity.status(e.getStatus()).body(e.getResponse());
+    }
+
+    @ExceptionHandler(UpdateEntityMismatchException.class)
+    public ResponseEntity handleUpdateEntityMismatch(Exception e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleEntityNotFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(CreateEntityMismatchException.class)
+    public ResponseEntity handleCreateEntityMismatch(Exception e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(EntityFoundException.class)
+    public ResponseEntity handleEntityFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(CacheDisabledException.class)
+    public ResponseEntity handleBadRequest(Exception e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(UnknownHostException.class)
+    public ResponseEntity handleUnkownHost(Exception e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(CacheNotFoundException.class)
+    public ResponseEntity handleCacheNotFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ErrorResponse.of(e));
+    }
+
+}
+
---
+++ src/main/java/no/novari/fint/consumer/models/tilgang/TilgangCacheService.java
@@ -0,0 +1,134 @@
+package no.novari.fint.consumer.models.tilgang;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import lombok.extern.slf4j.Slf4j;
+
+import no.fint.cache.CacheService;
+import no.fint.cache.model.CacheObject;
+import no.novari.fint.consumer.config.Constants;
+import no.novari.fint.consumer.config.ConsumerProps;
+import no.novari.fint.consumer.event.ConsumerEventUtil;
+import no.fint.event.model.Event;
+import no.fint.event.model.ResponseStatus;
+import no.novari.fint.relations.FintResourceCompatibility;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import no.novari.fint.model.arkiv.noark.Tilgang;
+import no.novari.fint.model.resource.arkiv.noark.TilgangResource;
+import no.novari.fint.model.arkiv.noark.NoarkActions;
+import no.novari.fint.model.felles.kompleksedatatyper.Identifikator;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(name = "fint.consumer.cache.disabled.tilgang", havingValue = "false", matchIfMissing = true)
+public class TilgangCacheService extends CacheService<TilgangResource> {
+
+    public static final String MODEL = Tilgang.class.getSimpleName().toLowerCase();
+
+    @Value("${fint.consumer.compatibility.fintresource:true}")
+    private boolean checkFintResourceCompatibility;
+
+    @Autowired
+    private FintResourceCompatibility fintResourceCompatibility;
+
+    @Autowired
+    private ConsumerEventUtil consumerEventUtil;
+
+    @Autowired
+    private ConsumerProps props;
+
+    @Autowired
+    private TilgangLinker linker;
+
+    private JavaType javaType;
+
+    private ObjectMapper objectMapper;
+
+    public TilgangCacheService() {
+        super(MODEL, NoarkActions.GET_ALL_TILGANG, NoarkActions.UPDATE_TILGANG);
+        objectMapper = new ObjectMapper();
+        javaType = objectMapper.getTypeFactory().constructCollectionType(List.class, TilgangResource.class);
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    }
+
+    @PostConstruct
+    public void init() {
+        props.getAssets().forEach(this::createCache);
+    }
+
+    @Scheduled(initialDelayString = Constants.CACHE_INITIALDELAY_TILGANG, fixedRateString = Constants.CACHE_FIXEDRATE_TILGANG)
+    public void populateCacheAll() {
+        props.getAssets().forEach(this::populateCache);
+    }
+
+    public void rebuildCache(String orgId) {
+		flush(orgId);
+		populateCache(orgId);
+	}
+
+    @Override
+    public void populateCache(String orgId) {
+		log.info("Populating Tilgang cache for {}", orgId);
+        Event event = new Event(orgId, Constants.COMPONENT, NoarkActions.GET_ALL_TILGANG, Constants.CACHE_SERVICE);
+        consumerEventUtil.send(event);
+    }
+
+
+    public Optional<TilgangResource> getTilgangBySystemId(String orgId, String systemId) {
+        return getOne(orgId, systemId.hashCode(),
+            (resource) -> Optional
+                .ofNullable(resource)
+                .map(TilgangResource::getSystemId)
+                .map(Identifikator::getIdentifikatorverdi)
+                .map(systemId::equals)
+                .orElse(false));
+    }
+
+
+	@Override
+    public void onAction(Event event) {
+        List<TilgangResource> data;
+        if (checkFintResourceCompatibility && fintResourceCompatibility.isFintResourceData(event.getData())) {
+            log.info("Compatibility: Converting FintResource<TilgangResource> to TilgangResource ...");
+            data = fintResourceCompatibility.convertResourceData(event.getData(), TilgangResource.class);
+        } else {
+            data = objectMapper.convertValue(event.getData(), javaType);
+        }
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
+        if (NoarkActions.valueOf(event.getAction()) == NoarkActions.UPDATE_TILGANG) {
+            if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
+                List<CacheObject<TilgangResource>> cacheObjects = data
+                    .stream()
+                    .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
+                    .collect(Collectors.toList());
+                addCache(event.getOrgId(), cacheObjects);
+                log.info("Added {} cache objects to cache for {}", cacheObjects.size(), event.getOrgId());
+            } else {
+                log.debug("Ignoring payload for {} with response status {}", event.getOrgId(), event.getResponseStatus());
+            }
+        } else {
+            List<CacheObject<TilgangResource>> cacheObjects = data
+                    .stream()
+                    .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
+                    .collect(Collectors.toList());
+            updateCache(event.getOrgId(), cacheObjects);
+            log.info("Updated cache for {} with {} cache objects", event.getOrgId(), cacheObjects.size());
+        }
+    }
+}
